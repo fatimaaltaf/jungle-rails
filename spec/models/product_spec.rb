5367e27790b99d77f44f946eb31393bf2522861f
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-16 23:58:11 +0000

    is valid when fields are filled and is not valid without name tests work

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe ProductSpec.rb, type: :model do
+RSpec.describe Product, type: :model do
   describe 'Validations' do
-  end 
+
+    before(:each) do
+      @category = Category.new(id: 1, name: 'Apparel')
+      @product = Product.new(name: "Bob", price: 10000, quantity: 4, category: @category)
+    end
+
+    it "is valid when all fields are filled" do
+      expect(@product).to be_valid
+      expect(@category).to be_valid
+    end 
+
+    it "is not valid without a name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+  end
 end
