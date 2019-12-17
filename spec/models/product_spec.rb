990b07a0c4baa7c4c749d42b76d0934fa2aec401
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-17 00:52:25 +0000

    not valid without price, quantity, and category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,5 +17,21 @@ RSpec.describe Product, type: :model do
       @product.name = nil
       expect(@product).to_not be_valid
     end
+
+    it "is not valid without a price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end
+
   end
 end
