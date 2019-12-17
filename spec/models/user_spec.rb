e06ad2858168493f13892dee791f88c1bdc5a837
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-17 18:33:46 +0000

    allows one email address to register and sign up with case sensitive emails test pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,5 +50,24 @@ RSpec.describe User, type: :model do
       @user.password = "aAA"
       expect(@user).to_not be_valid
     end
+
+    it "only allows one email address to register" do
+      @user.save
+      @user1 = User.new(first_name: "Polly", last_name: "Parrot", email: @user.email, password: "abcde", password_confirmation: "abcde")
+      @user1.save
+      expect(@user1.errors.messages[:email][0]).to eq("has already been taken")
+    end
+
+    it "is invalid when user tries to sign up with case sensitive email" do
+      @user.save
+      @user1 = User.new(first_name: "Polly", last_name: "Parrot", email: "POLLY.PARROT@gmail.com", password: "abcde", password_confirmation: "abcde")
+      @user1.save
+      expect(@user1.errors.messages[:email][0]).to eq("has already been taken")
+    end
   end
+
+  # describe '.authenticate_with_credentials' do
+  #   before(:each) do
+  #     @user = User.create(id: 1, first_name: "homer", last_name: "simpson", email: "homer@simpson.com", password: "aaa", password_confirmation: "aaa")
+  #   end
 end
