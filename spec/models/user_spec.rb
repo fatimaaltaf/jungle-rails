72d1830f1a265d301ebc5f4d7498297a132d0735
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-17 01:56:01 +0000

    invalid without first name and last name tests pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,21 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
     before(:each) do
-      @user = User.new(first_name: "Polly", last_name: "Parrot", email: 'polly.parrot@gmail.com', password: 'qwerty123', password_confirmation: 'qwerty123')
+      @user = User.new(id: 1, first_name: "Polly", last_name: "Parrot", email: 'polly.parrot@gmail.com', password: 'qwerty123', password_confirmation: 'qwerty123')
     end 
 
     it "is valid when all fields are filled" do
       expect(@user).to be_valid
+    end
+
+    it "is invalid without first_name" do
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is invalid without last_name" do
+      @user.last_name = nil
+      expect(@user).to_not be_valid
     end
   end
 end
